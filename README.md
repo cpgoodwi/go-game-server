3e7003f2a38ec433b92ee981f4c1a8425f5076ee
CharleyIsCool <cpgoodwi@asu.edu>
2025-02-21 14:08:52 -0700

    set up simple server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GoLang Game Server
 Full stack web-based game server
+
+## TODO:
+[ ] - implement auth
+[ ] - implement hub/room system
+[ ] - users move between rooms
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Initializing Game Server")
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello world")
+	})
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	fmt.Println("Server starting on port 8080")
+
+	log.Fatal(server.ListenAndServe())
 }
