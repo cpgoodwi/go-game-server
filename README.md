0d45504e19187c0cd5a6481f70a0b9a51f0f9c23
CharleyIsCool <cpgoodwi@asu.edu>
2025-02-21 15:08:20 -0700

    added templating

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 Full stack web-based game server
 
 ## TODO:
+[x] - initialize go server
+[ ] - user postgres database
 [ ] - implement auth
 [ ] - implement hub/room system
 [ ] - users move between rooms
+[x] - serve template
+[ ] - integrate frontend framework
--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+var (
+	templates *template.Template
+)
+
+func init() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
+}
 
 func main() {
 	fmt.Println("Initializing Game Server")
 
 	router := http.NewServeMux()
 
+	fs := http.FileServer(http.Dir("static"))
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello world")
+		data := struct{}{}
+		templates.ExecuteTemplate(w, "index.html", data)
 	})
 
 	server := http.Server{
@@ -20,7 +33,7 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Println("Server starting on port 8080")
+	fmt.Println("Server starting on http://localhost:8080")
 
 	log.Fatal(server.ListenAndServe())
 }
